8c5c2a722c51aeeec863d23d95e8a58a247e9b69
radharukminiY <rukku558@gmail.com>
2021-02-09 22:20:07 +0530

    commit 4

---
+++ src/components/Listing/listingApi.js
@@ -0,0 +1,48 @@
+import React,{Component} from 'react';
+import axios from 'axios';
+import ListingDisplay from './listingDisplay';
+import Header from '../../Header';
+import CuisineFilter from '../filters/cuisineFilter';
+import CostFilter from '../filters/CostFilter';
+
+const url = "https://eduintern.herokuapp.com/rest?mealtype=";
+
+class ListingApi extends Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+            restlist:''
+        }
+    }
+
+  setDataFilter=(sorteData) =>{
+    this.setState({restlist:sortedData})
+  }
+  render(){
+    console.log(">>>",this.state.restlist)
+    return(
+      <div className="row">
+      </Header>
+      <div style={{marginLeft:'5%'}}>
+
+      <div className="col-md-2">
+      <CuisineFilter restPerCuisine={(data) => {this.setDataPerFilter(data)}}/>
+      <CostFilter restPerCost={(data) =>{this.setDataFilter(data)}}/>
+      </div>
+      <div className="col-md-10">
+      <ListingDisplay restaurentList={this.state.restlist}/>
+      </div>
+      </div>
+      </div>
+    )
+  }
+
+  componentDidMount(){
+    var mealid =this.props.match.params.id
+    sessionStorage.setItem('mealId',mealid)
+    axios.get(`${url}${mealid}`)
+    .then((response) =>{this.setState({restlist:response.data})})
+
+  }
+  export default ListingApi;
